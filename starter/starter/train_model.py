f903b34aed6b50ebba9ad19fb3fd5bf71171319d
leohsuofnthu <leohsuofnthu@gmail.com>
2021-12-25 00:51:45 -0500

    model traning script finish

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -32,7 +32,12 @@ X_train, y_train, encoder, lb = process_data(
 
 # Proces the test data with the process_data function.
 X_test, y_test, encoder, lb = process_data(
-    test, categorical_features=cat_features, label="salary", training=False
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb,
 )
 # Train and save a model.
 trained_model = train_model(X_train, y_train)
