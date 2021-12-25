3999c1ec92cf673d6cac5006db673e6170b7e325
leohsuofnthu <leohsuofnthu@gmail.com>
2021-12-25 14:42:29 -0500

    fix path

--- conftest.py
+++ conftest.py
@@ -1,1 +1,65 @@
-# Help pytest to find the root path
+import os
+import pytest
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from starter.starter.ml.data import process_data
+from starter.starter.ml.model import train_model
+
+
+@pytest.fixture()
+def data():
+    """10 sample data for test"""
+
+    dirname = os.path.dirname(__file__)
+    df = pd.read_csv(os.path.join(dirname, "starter/data/cleaned_census.csv"))[:10]
+    return df
+
+
+@pytest.fixture()
+def processedTrain(data):
+    """processed data for train/test"""
+
+    train, test = train_test_split(data, test_size=0.20)
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+    X_test, y_test, encoder, lb = process_data(
+        test,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )
+
+    res = {
+        "train": train,
+        "test": test,
+        "X_train": X_train,
+        "y_train": y_train,
+        "X_test": X_test,
+        "y_test": y_test,
+        "encoder": encoder,
+        "lb": lb,
+    }
+    return res
+
+
+@pytest.fixture()
+def trainedModel(processedTrain):
+    """simple model for testing"""
+
+    X_train = processedTrain["X_train"]
+    y_train = processedTrain["y_train"]
+    trained_model = train_model(X_train, y_train)
+    return trained_model
