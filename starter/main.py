42a41db1081b744af07f9f1d799a9e1862cf6c69
leohsuofnthu <leohsuofnthu@gmail.com>
2021-12-25 22:38:00 -0500

    add apt file

--- starter/main.py
+++ starter/main.py
@@ -6,6 +6,13 @@ import joblib
 import numpy as np
 import pandas as pd
 
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 app = FastAPI()
 
 
