f8cc3366056913c1d8dce444a8661b3f1fd49bf7
leohsuofnthu <leohsuofnthu@gmail.com>
2021-12-25 23:44:06 -0500

    fix path for dvc pull

--- starter/main.py
+++ starter/main.py
@@ -7,11 +7,11 @@ import numpy as np
 import pandas as pd
 
 
-if "DYNO" in os.environ and os.path.isdir(".dvc"):
+if "DYNO" in os.environ and os.path.isdir("../dvc"):
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
+    os.system("rm -r ../dvc .apt/usr/lib/dvc")
 
 app = FastAPI()
 
